3992ded47c7f0a18b83edaad08d701b682c9d5dd
james.jiang <james.jiang@dianping.com>
2015-01-21 07:58:14 +0800

    add controller & utils

--- src/main/java/com/wx/local/Controller.java
+++
@@ -1,5 +0,0 @@
-package com.wx.local;
-
-public class Controller {
-
-}
---
+++ src/main/java/com/wx/local/controller/ConnectController.java
@@ -0,0 +1,54 @@
+package com.wx.local.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collections;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.google.common.collect.Lists;
+import com.wx.local.utils.CommonUtils;
+
+@Controller
+public class ConnectController {
+	Logger logger = Logger.getLogger(getClass());
+
+	@RequestMapping("/yx/connect")
+	public void testConnect(String signature, String timestamp, String nonce,
+			String echostr, HttpServletResponse respnose) throws IOException {
+		logger.info(signature + "," + timestamp + "," + nonce + "," + echostr);
+		List<String> params = Lists.newArrayList();
+		params.add("123token321");
+		params.add(timestamp);
+		params.add(nonce);
+		logger.info(params);
+		Collections.sort(params);
+		logger.info(params);
+		String paramsStr = list2String(params);
+		String encrytpStr = sha1Encoding(paramsStr);
+		// logger.info(paramsStr);
+		logger.info(signature + " SHA1 match ? " + signature.equals(encrytpStr)
+				+ " " + encrytpStr);
+		PrintWriter writer = respnose.getWriter();
+		writer.write(echostr);
+	}
+
+	private String sha1Encoding(String paramsStr) {
+		return CommonUtils.encrypt(paramsStr, "SHA-1");
+
+	}
+
+	private String list2String(List<String> source) {
+		StringBuilder sb = new StringBuilder();
+		for (String s : source) {
+			sb.append(s);
+		}
+		return sb.toString();
+	}
+
+}
