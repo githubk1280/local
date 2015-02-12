bff0ff2b3fb0293b3c53a64fbceb5fa0f5794460
james.jiang <james.jiang@dianping.com>
2015-02-12 20:07:54 +0800

    update

--- src/main/java/com/wx/local/service/handler/event/impl/SubscribeEventHandlerImpl.java
+++ src/main/java/com/wx/local/service/handler/event/impl/SubscribeEventHandlerImpl.java
@@ -8,25 +8,24 @@ import com.wx.local.beans.Image;
 import com.wx.local.beans.Xml;
 import com.wx.local.config.WXConfig.MessageTypeEnum;
 import com.wx.local.service.handler.MessageHandler;
-import com.wx.local.utils.LoggerUtils;
 
 @Component("subscribeEventHandlerImpl")
 public class SubscribeEventHandlerImpl implements MessageHandler {
 
 	@Override
-	public Xml handle(final Xml xml) {
+	public Xml handle(Xml xml) {
 		logger.info("subscribe bingo..........");
-		logger.info(String.format(LoggerUtils.URL_COUNT_LOGGER_TEMPLATE,
-				xml.getEventKey()));
+		Xml returnXml = new Xml();
 		String to = xml.getFromUserName();
 		String from = xml.getToUserName();
-		xml.setCreateTime(new Date().getTime());
-		xml.setFromUserName(from);
-		xml.setToUserName(to);
-		xml.setMsgType(MessageTypeEnum.image.name());
+		returnXml.setCreateTime(new Date().getTime());
+		returnXml.setFromUserName(from);
+		returnXml.setToUserName(to);
+		returnXml.setMsgType(MessageTypeEnum.image.name());
 		Image image = new Image();
 		image.setMediaId("203716587");
-		xml.setImage(image);
-		return xml;
+		returnXml.setImage(image);
+		logger.info(returnXml);
+		return returnXml;
 	}
 }
